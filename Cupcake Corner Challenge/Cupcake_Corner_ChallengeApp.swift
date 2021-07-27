e86084994a3ed4b2fd8121417ea3013c76f2a9aa
Olivier Van hamme <nicefiction@gmail.com>
2021-07-27 09:33:50 +0200

    Initial Commit

---
+++ Cupcake Corner Challenge/Cupcake_Corner_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_Corner_ChallengeApp.swift
+//  Cupcake Corner Challenge
+//
+//  Created by Olivier Van hamme on 27/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_Corner_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
